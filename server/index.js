9dec77a03f4864c734102d06cdb77aa0eaa3a305
shivang <shivangnov@gmail.com>
2023-03-30 12:51:35 +0530

    added schemas

--- server/index.js
+++ server/index.js
@@ -4,9 +4,11 @@ app.use(express.json())
 const mongoose = require("mongoose")
 const dotenv = require("dotenv");
 const { connectDB } = require("./databases/database");
-
+const userRoutes = require("./routes/userRoutes")
+const policeRoutes = require("./routes/policeRoutes")
 dotenv.config();
-
+app.use('/api/police',policeRoutes)
+app.use('/api/users',userRoutes)
 
 app.listen(process.env.PORT, () => {
     console.log("Backend Server is running !")
